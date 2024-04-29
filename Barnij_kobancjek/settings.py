e3fd15e501f0e64391e0a4630cd0562596ac8b10
Max Gatskan <isip_m.n.gackan@mpt.ru>
2024-04-29 13:20:29 +0300

    url

--- Barnij_kobancjek/settings.py
+++ Barnij_kobancjek/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Barnij_kobancjek.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 # Password validation
